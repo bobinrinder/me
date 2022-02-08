9a05328f2a2fbc366787c51f8f2bcdc43f3b38ee
Robin Binder <bobinrinder@users.noreply.github.com>
2022-02-08 09:38:12 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -34,7 +34,7 @@ const About = () => {
             <Table>
               <tbody>
                 <tr>
-                  <td>2020 - 2021</td>
+                  <td>2020 - 2022</td>
                   <td>
                     Head of Engineering at{" "}
                     <ExternalLink href="https://m56studios.com">
