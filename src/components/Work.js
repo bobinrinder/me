fc6bd45eb40bcae45b77a341ab3b8e5470644696
Robin Binder <bobinrinder@users.noreply.github.com>
2022-03-04 13:41:30 -0800

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -55,7 +55,7 @@ const Work = () => {
                     <tbody>
                       <tr>
                         <td className="text-white border-0">Users</td>
-                        <td className="text-white border-0">&gt; 250k</td>
+                        <td className="text-white border-0">&gt; 450k</td>
                       </tr>
                       <tr>
                         <td className="text-white">Stack</td>
