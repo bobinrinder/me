9ea624a1243fd18185f9ea2a9aa95418184f8623
Robin Binder <bobinrinder@users.noreply.github.com>
2022-05-24 03:58:51 +0000

    fixed height attribute for store buttons

--- src/components/PlayStoreButton.js
+++ src/components/PlayStoreButton.js
@@ -3,12 +3,14 @@ import React from "react"
 const PlayStoreButton = ({
   href,
   width = 190,
+  height = 74,
   alt = "Get it on Google Play",
 }) => {
   return (
     <a href={href} target="_blank" rel="noopener noreferrer">
       <img
         width={width}
+        height={height}
         alt={alt}
         src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
       />
